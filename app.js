2dc3bc56fe33473e7ba33595f85ad7d30afc1148
Bruno Adriano <adr12d00@gmail.com>
2025-02-03 21:10:16 -0300

    ultima alteração

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+let amigo;
+
+const campoAmigo = document.getElementById("amigo");
+const listaAmigos = document.getElementById("listaAmigos");
+const resultado = document.getElementById("resultado");
+
+
+function adicionarAmigo(){
+        if (resultado.textContent !== ""){
+            listaAmigos.innerHTML = "";
+            resultado.innerHTML = "";
+            amigos = []
+        }
+
+        amigo = campoAmigo.value == "" ? prompt('Por favor insira um nome'): campoAmigo.value;
+        amigos.push(amigo);
+
+        campoAmigo.value = "";
+
+        atualizarAmigos()
+}
+
+function atualizarAmigos(){
+
+    listaAmigos.innerHTML = "";
+    amigos.forEach((a)=>{
+
+        let item = document.createElement("li");
+        item.innerText = a;
+
+        listaAmigos.appendChild(item);
+    })
+}
+
+function sortearAmigo(){
+        if (amigos.length > 0){
+            let indice = Math.floor(Math.random() * amigos.length);
+            resultado.innerHTML = `O amigo sorteado foi ${amigos[indice]}`;
+        } else {
+            adicionarAmigo()
+        }
+}
